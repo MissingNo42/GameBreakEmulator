be1a1b3c598dc47daaeea4e8a5e861ad29f02f2e
MissingNo42 <romainklodzinski@live.com>
2023-08-17 16:55:12 +0200

    add emulator components' reset & states mgr macros

--- src/utils.h
+++ src/utils.h
@@ -5,12 +5,28 @@
 #ifndef GOBOUEMU_UTILS_H
 #define GOBOUEMU_UTILS_H
 
+
+////////////////////////   Macros   ///////////////////////////
+
+#define Reset(name) void inline Reset_##name()
+#define SaveSize(name, size) u16 inline SaveSize_##name() { return size; }
+#define Save(name) void inline Save_##name()
+#define Load(name) void inline Load_##name()
+#define save_obj(obj, sz)
+#define load_obj(obj, sz)
+
+
+////////////////////////    Types   ///////////////////////////
+
 typedef enum {
 	Debug    = 32,
 	Info     = 112,
 	Error    = 208,
 	Critical = 196
 } ELOG;
+
+
+////////////////////////   Methods   ///////////////////////////
 
 void Log(ELOG type, char * title, char * str, ...);
 
