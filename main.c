f57ad136664f1dd543cfc7bca152c0582b8db7e1
MissingNo42 <romainklodzinski@live.com>
2023-08-20 17:56:39 +0200

    preparation of bootrom test + refactor + code cleanup

--- main.c
+++ main.c
@@ -1,22 +1,40 @@
 #include <stdio.h>
-#include "src/units/mmu.h"
+#include <malloc.h>
 #include "src/cartridge.h"
 #include "src/mapper.h"
+#include "src/io_ports.h"
+#include "src/units/mmu.h"
 #include "src/units/cpu.h"
+#include "src/units/dma.h"
+#include "src/units/ppu.h"
 
 #define tr "../tloz.gb"
 //#define tr "../cpu_instrs.gb"
-#include <malloc.h>
 
 int main() {
-	INFO("GobouEmulator starting", "cartridge = %s\n", tr);
-	load_header(tr);
-	init_memory();
-	INFO("Memory loaded", "@ %p\n", Memory);
-	init_mapper();
+	INFO("GobouEmulator starting", "cartridge file = %s\n", tr);
 	
-	INFO("Load cartridge...", "\n");
-	load_cartridge(tr);
+	open_cartridge(tr);
+	
+	Reset_io_ports(1);
+	Reset_cpu(1);
+	Reset_ppu(1);
+	Reset_dma(1);
+	
+	Reset_cartridge(1);
+	Reset_mmu(1);
+	Reset_mapper(1);
+	
+	/*
+	if (open_cartridge(tr)
+	&& load_header()
+	&& init_memory()) {
+		INFO("Memory loaded", "@ %p\n", Memory);
+		init_mapper();
+		
+		INFO("Load cartridge...", "\n");
+		load_cartridge();
+	}*/
 	
 	/* approx mapper1 test
 	u8 t[ROM1];
@@ -45,10 +63,27 @@ int main() {
 		if (42!=read(i)) ERROR("Ram fault", "%04X %hhu\n", i, read(i));
 	}*/
 	
+	INFO("rom:", " %p\nenter:", memoryMap.rom0+ROM0);
+	getchar();
+	
 	INFO("Gobou Start !", "\n");
-	cpu_init();
-	for (u32 i = 0; i < (4194304<<8); i++) {
-		//DEBUG("I", "%d\n", i);
+	//cpu_init();
+	//for (u32 i = 0; i < (4194304<<8); i++) {
+	
+	for (u32 i = 0; i < (4000000 * 100); i++) {
+		//if (i > 47000 && ioLY > 0x88 && ioLY < 0x92)
+		//	DEBUG("\t", "%d \t A %02X (z %hhu) | C %02X | E %02X | HL %04X ( LY %hhu 0x%02X)\n", i, A, z, C, E, HL,ioLY, ioLY);
+		if (!(i & 0xffff))
+			DEBUG("\t", "%d \t A %02X (z %hhu) | C %02X | E %02X | HL %04X ( LY %hhu 0x%02X)\n", i, A, z, C, E, HL,ioLY, ioLY);
+		cpu_run();
+		if (PC > 0x100) break;
+	}
+	
+	char xx=0;
+	for (u32 i = 0; i < 20000000; i++) {
+		//if (!(i & 0xffff))
+		if (i > 100) xx=1;
+		//if (xx)DEBUG("\t", "%d \t A %02X (z %hhu) | C %02X | E %02X | HL %04X ( LY %hhu 0x%02X)\n", i, A, z, C, E, HL,ioLY, ioLY);
 		cpu_run();
 	}
 	INFO("Gobou Exit !", "\n");
