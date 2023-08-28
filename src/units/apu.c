44e7e45943ed7c6b90787b895e693101698c0107
MissingNo42 <romainklodzinski@live.com>
2023-08-28 21:44:03 +0200

    add apu files for sound emulation (todo)

---
+++ src/units/apu.c
@@ -0,0 +1,5 @@
+//
+// Created by Romain on 26/08/2023.
+//
+
+#include "apu.h"
---
+++ src/units/apu.h
@@ -0,0 +1,8 @@
+//
+// Created by Romain on 26/08/2023.
+//
+
+#ifndef GBEMU_APU_H
+#define GBEMU_APU_H
+
+#endif //GBEMU_APU_H
