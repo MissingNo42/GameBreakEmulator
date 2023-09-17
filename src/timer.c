0d26d653f3550714597419e984255f6e18d15ff1
MissingNo42 <romainklodzinski@live.com>
2023-09-17 21:59:45 +0200

    timer opti (profiler suggestion?) + prepare reimpl. since not any test rom pass

--- src/timer.c
+++ src/timer.c
@@ -9,86 +9,40 @@ const u16 divmask[] = {0x200, 0x8, 0x20, 0x80};
 
 Timer timer;
 
-void upd(u8 value, u8 tma) {
-	if (tma) ioTMA = value;
-	u8 itima = ioTIMA++;
-	u8 carry = !itima;
-	u8 old_carry;
-	
-	u8 nt;
-	if (tma) {
-		nt = value;
-	} else nt = itima;
-	
-	u8 flag = old_carry && !tma;
-	if (flag) ioIF |= INT_TIMER;
-	if (flag || tma) {
-		nt = ioTMA;
-	}
-	
-}
-
-void clock_run2(u8 cycles) {
-	if (cycles != 4 && cycles != 8) CRITICAL("", "g\n");
-	do {
-		timer.wdiv++;
-		ioDIV = timer.div;
-		
-		fed_set(timer.snd_fed, (timer.div >> ((double_speed) ? 6 : 5)) & 1);
-		if (fed_detect(timer.snd_fed)) // TODO timer sound
-			(void)0;
-		
-		if (timer.reset_tima_done){
-			if (timer.reset_tima_delay--) {
-				timer.reset_tima_done = 0;
-			}
-		}
-		
-		
-		if (timer.reset_tima_rq){ // overflow reset delayed
-			if (timer.reset_tima_delay--) {
-				timer.reset_tima_rq = 0;
-				timer.reset_tima_done = 1;
-				ioTIMA = ioTMA;
-				add_interrupt(INT_TIMER);
-			}
-		}
-		
-		fed_set(timer.tma_fed, (ioTAC & 4) &&      // TAC.Enable
-		       (timer.wdiv & divmask[ioTAC & 3])); // TAC.Freq
-		if (fed_detect(timer.tma_fed)) {
-			ioTIMA++;
-			timer.reset_tima_rq = !ioTIMA;
-		}
-	} while(--cycles);
-}
-
 
 void clock_run(u8 cycles) {
-	if (cycles != 4 && cycles != 8) CRITICAL("", "g\n");
+	//if (cycles != 4 && cycles != 8) CRITICAL("", "g\n");
+	
 	cycles >>= 2;
+	s32 snd_mask = double_speed ? 0x40 : 0x20;
+	s32 tma_mask = (ioTAC & 4) ? divmask[ioTAC & 3]: 0;
+	
 	do {
 		timer.wdiv += 4;
 		ioDIV = timer.div;
 		
-		fed_set(timer.snd_fed, (timer.div >> ((double_speed) ? 6 : 5)) & 1); // rework for m-Cycle
-		if (fed_detect(timer.snd_fed)) // TODO timer sound
-			(void)0;
+		s32 clock_fed = timer.div & snd_mask;
+		
+		if (timer.snd_fed && !clock_fed) {
+			timer.snd_fed = 0;
+			 // TODO timer sound
+		} else timer.snd_fed = clock_fed;
 		
 		if (timer.reset_tima_done) timer.reset_tima_done = 0;
 		
-		if (timer.reset_tima_rq){ // overflow reset delayed
+		if (timer.reset_tima_rq){ // overflow reset delay (instead of !ioTIMA because 0x00 write dont reset TIMA)
 			timer.reset_tima_rq = 0;
 			timer.reset_tima_done = 1;
 			ioTIMA = ioTMA;
 			add_interrupt(INT_TIMER);
 		}
 		
-		fed_set(timer.tma_fed, (ioTAC & 4) &&      // TAC.Enable
-		       (timer.wdiv & divmask[ioTAC & 3])); // TAC.Freq
-		if (fed_detect(timer.tma_fed)) {
+		clock_fed = timer.wdiv & tma_mask;
+		if (timer.tma_fed && !clock_fed) {
+			timer.tma_fed = 0;
 			ioTIMA++;
-			timer.reset_tima_rq = !ioTIMA; // Overflow
-		}
+			if (!ioTIMA) timer.reset_tima_rq = 1; // Overflow
+		} else timer.tma_fed = clock_fed;
+		
 	} while(--cycles);
 }
--- src/timer.h
+++ src/timer.h
@@ -8,29 +8,20 @@
 #include "types.h"
 #include "io_ports.h"
 
-#define ClkPerSec 4194304 // 1 << 22
+// #define ClkPerSec 4194304 // 1 << 22
 
 Struct {
 	union {
-		struct {
-#ifdef IS_LITTLE_ENDIAN
-			u8 _, div;
-#else
-			u8 div, _;
-#endif
-		};
+		Estruct2(u8, _, div)
 		u16 wdiv;
 	};
-	u8 snd_fed: 2, tma_fed: 2; // FED: falling edge detector
-	u8 reset_tima_rq: 1;
-	u8 reset_tima_done: 1;
-	u8 reset_tima_delay: 2;    // = 3 default
+	u8 reset_tima_rq;
+	u8 reset_tima_done;
+	u8 snd_fed;
+	u16 tma_fed; // FED: falling edge detector
 } Timer;
 
 extern Timer timer;
-
-#define fed_detect(fed) ((fed) == 0x02)
-#define fed_set(fed, value) fed = ((fed) << 1) | (value)
 
 void clock_run(u8 cycles);
 
@@ -40,7 +31,6 @@ Reset(timer) {
 	timer.tma_fed = 0;
 	timer.reset_tima_rq = 0;
 	timer.reset_tima_done = 0;
-	timer.reset_tima_delay = 3;
 }
 
 SaveSize(timer, sizeof(Timer))
